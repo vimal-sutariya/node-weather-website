8e588065a5286a3645ce5681408c46c91e766377
Vimal Sutariya <vimal.sutariya67@gmail.com>
2023-09-02 21:14:54 -0600

    Set up changes for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ weatherForm.addEventListener('submit', (e) => {
     
     const location = search.value;
     
-    const url = 'http://localhost:3000/weather?address=' + location + '';
+    const url = '/weather?address=' + location + '';
     
     msgOne.textContent = 'Loading...';
     msgTwo.textContent = '';
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -64,6 +65,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
 })
